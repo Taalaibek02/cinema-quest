63e368f25da4450aeba90e90b587d4aa9071d597
iBlameTaalai <taalaibek02@gmail.com>
2024-05-31 16:24:26 -0500

    First commit

---
+++ movies/src/main/java/dev/taalai/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.taalai.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
